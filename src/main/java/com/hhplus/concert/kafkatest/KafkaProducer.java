bd429aa51fed5069d6b5d501ce538f6a0df85caf
won <jwon3991.9191@gmail.com>
2024-11-22 03:34:13 +0900

    feat:  consumer, producer 를 연동 및 테스트

---
+++ src/main/java/com/hhplus/concert/kafkatest/KafkaProducer.java
@@ -0,0 +1,20 @@
+package com.hhplus.concert.kafkatest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+        log.info("Sent message: {}", message);
+    }
+}
